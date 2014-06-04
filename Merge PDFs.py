68d2702b8b6b96c2a140934a5941fa2c7962852e
Geoff Russell <Geoff.R.Russell@gmail.com>
2014-06-04 11:45:59 -0700

    Pulls out all part and inspection standard filenames. Matching of the part drawing to the inspection standard.

--- Merge PDFs.py
+++ Merge PDFs.py
@@ -10,23 +10,40 @@ Output
 
 from PyPDF2 import PdfFileMerger
 import os
+import re
+import sys
 
 merger = PdfFileMerger()
 
 dir = "D:\\Desktop\\ECO80476 - Tip and Shaft"
 #dir = raw_input("What is the directory?") #Assumes both part drawings and inspection standards are in the same directory
-startPart = raw_input("What is the first part number (ex P24608)? ")
-numParts = int(raw_input("What is the total number of parts? "))
 #startPage = int(raw_input("Which page should the inspection standard start on? "))
+
+partFilename = []
+inspFilename = []
+
+expression = 'P\d+_\D'
+#Matches only part numbers at the beginning of the string
+#'P' Must start with P
+#'\d' Matches 1 or more decimal digits
+#'_' Matches underscore
+#'\D' Matches any nondigit character
 
 for filename in os.listdir(dir):
     root, ext = os.path.splitext(filename)
-    if root.startswith("P2461") and ext == ".pdf":
-        print filename
-        #merge the pdfs here
-        #Assume that all the files are paired together and combine
+
+    if re.match(expression, root) and ext == ".pdf": #Checks to see that it starts with the expected part number and rev and is a PDF
+        if ("Inspection Standard" in root):
+#             print "This is an inspection standard %s" %filename
+            inspFilename.append(filename)
+        else:
+#             print "This is a drawing %s" %filename
+            partFilename.append(filename)
+
+for part in partFilename:
+    for insp in inspFilename:
+        if part[:8] == insp[:8]: #Matches part and rev to merge
+            print "Merged %s and %s" %(part, insp)
 
 
-# for name in range(int(startPart[-5:]), int(startPart[-5:]) + numParts):
-#     fileLoc = dir + "P" + name
-#     part = open()
+
